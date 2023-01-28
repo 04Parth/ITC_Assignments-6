35df71a6b15c31a37daeb9e2a0ac3d096924cfbc
Parth Goyal <121252708+04Parth@users.noreply.github.com>
2023-01-29 02:38:02 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,9 +1,9 @@
-i = 1
-x = int(input("Enter the number: "))
-for _ in range(2, x):
-    if x % _ == 0:
-        i += _
-if x == i:
-    print("It is a perfect number")
+n = int(input("Enter any number: "))
+sum1 = 0
+for i in range(1, n):
+    if n % i == 0:
+        sum1 = sum1 + i
+if sum1 == n:
+    print("The number is a Perfect number!")
 else:
-    print("Not a perfect number")
+    print("The number is not a Perfect number!")
